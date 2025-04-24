11b00cd935eade0f5c365a5f5cc489c5873ac768
yRaposo <ynyght@gmail.com>
2025-04-24 11:25:32 -0300

    feat: add NavBar component with dynamic title and scroll effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,64 @@
+'use client';
+import React from "react";
+import { useState } from "react";
 import Image from "next/image";
+import Titulo1 from "@/../public/imgs/Titulo1.png";
+import Titulo2 from "@/../public/imgs/Titulo2.png";
+import Titulo3 from "@/../public/imgs/Titulo3.png";
+import Titulo4 from "@/../public/imgs/Titulo4.png";
+import Titulo5 from "@/../public/imgs/Titulo5.png";
+import Titulo6 from "@/../public/imgs/Titulo6.png";
+import Titulo7 from "@/../public/imgs/Titulo7.png";
+import NavBar from "@/components/NavBar";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  function getRandomTitulo() {
+    const titulos = [Titulo1, Titulo2, Titulo3, Titulo4, Titulo5, Titulo6, Titulo7];
+    return titulos[Math.floor(Math.random() * titulos.length)];
+  }
+
+  const [randomTitulo, setRandomTitulo] = useState(getRandomTitulo());
+
+  return (
+
+    <div className="flex flex-col items-center justify-center min-h-screen min-w-screen bg-gradient-to-b from-blue-500 via-blue-500 to-white">
+
+      <div className="flex flex-col items-center justify-center min-h-screen min-w-screen bg-gradient-to-b from-blue-500 via-blue-500 to-white">
+        <NavBar />
+        <div className="flex flex-row items-center justify-center min-h-screen min-w-screen"
+          style={{
+            backgroundImage: `url(/imgs/Eu.png)`, backgroundSize: 'cover', backgroundPosition: 'left 150px top 150px',
+            backgroundRepeat: "no-repeat"
+          }}>
+          <div className="flex flex-col items-start justify-center min-w-screen pl-50">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={randomTitulo}
+              alt="Eu"
+              width={500}
+              onMouseOver={() => setRandomTitulo(getRandomTitulo())}
+              onMouseOut={() => setRandomTitulo(getRandomTitulo())}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <p className="text-xl">Desenvolvedor Full-Stack • UI Designer • Editor de Video</p>
+            <div>
+              <image
+                src="/imgs/Logo.png"
+                alt="Logo"
+                width={50}
+                className="absolute bottom-0 right-0 mr-10 mb-10"
+              />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      </div>
+        <div className="flex flex-col items-center justify-center min-h-screen min-w-screen bg-gradient-to-b bg-[#0F1020]">
+          <h1 className="text-4xl font-bold mb-4 text-[#447EF2]">Sobre Mim</h1>
+          <p className="text-lg text-center max-w-2xl text-white">
+            Olá! Meu nome é [Seu Nome] e sou um desenvolvedor Full-Stack apaixonado por criar experiências digitais incríveis. Com habilidades em design de interface do usuário e edição de vídeo, estou sempre em busca de novos desafios e oportunidades para aprender e crescer na área de tecnologia.
+          </p>
+        </div>
     </div>
+
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,54 @@
+'use client'
+
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import Image from "next/image";
+import Titulo1 from "@/../public/imgs/Titulo1.png";
+import Titulo2 from "@/../public/imgs/Titulo2.png";
+import Titulo3 from "@/../public/imgs/Titulo3.png";
+import Titulo4 from "@/../public/imgs/Titulo4.png";
+import Titulo5 from "@/../public/imgs/Titulo5.png";
+import Titulo6 from "@/../public/imgs/Titulo6.png";
+import Titulo7 from "@/../public/imgs/Titulo7.png";
+
+
+export default function NavBar() {
+    const [randomTitulo, setRandomTitulo] = useState(getRandomTitulo());
+    const [isAtTop, setIsAtTop] = useState(false);
+
+    function getRandomTitulo() {
+        const titulos = [Titulo2, Titulo3, Titulo4, Titulo6];
+        return titulos[Math.floor(Math.random() * titulos.length)];
+    }
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsAtTop(window.scrollY > 300); // Verifica se está no topo
+        };
+
+        window.addEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <div>
+            <nav className={`fixed top-0 left-0 w-full z-50 transition-colors duration-500 ${isAtTop === true ? 'bg-[#0F1020]' : 'bg-none'}`}>
+                <div className={`container mx-auto px-4 py-3 flex justify-between items-center `}>
+                    <div className="text-lg font-bold">
+                        <div className={`transition-opacity duration-500 ${isAtTop === true ? 'opacity-100' : 'opacity-0'}`}>
+                            <Image src={randomTitulo} alt="Random Titulo" width={250} />
+                        </div>
+                    </div>
+                    <div className={`flex space-x-4 transition-colors font-medium duration-500 ${isAtTop === true ? 'text-white' : 'text-black'}`}>
+                        <a href="#sobre" className="hover:text-[#5AFF15]">Sobre</a>
+                        <a href="#contato" className="hover:text-[#5AFF15]">Habilidades</a>
+                        <a href="#curriculo" className="hover:text-[#5AFF15]">Portfólio</a>
+                    </div>
+                </div>
+            </nav>
+            <div className="pt-16">
+
+            </div>
+        </div>
+    )
+}
