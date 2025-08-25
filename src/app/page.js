6e86ff1a8ceaedb9d1a491dd6c0cf305f983e88f
yRaposo <ynyght@gmail.com>
2025-08-25 16:25:58 -0300

    fix: ajusta a ordem de exibição e margens na seção de Soft Skills para melhor layout responsivo

--- src/app/page.js
+++ src/app/page.js
@@ -162,9 +162,9 @@ export default function Home() {
       </section>
 
       {/* Soft Skills Section */}
-      <section className="flex flex-col lg:flex-row items-start justify-center w-full pt-[160px] px-[20px] md:px-[64px]" id="softSkills">
+      <section className="flex flex-col-reverse lg:flex-row items-start justify-center w-full pt-[160px] px-[20px] md:px-[64px]" id="softSkills">
         <div className="flex w-full lg:w-fit h-fit items-center justify-center">
-          <div className="h-fit w-fit lg:mr-10 border-1 border-[#f5f5f5] p-2 relative mb-10 lg:mb-0">
+          <div className="h-fit w-fit lg:mr-10 border-1 border-[#f5f5f5] p-2 relative mt-10 lg:mb-0">
             <div className="h-fit w-fit relative"
               style={{
                 backgroundImage: "url(/imgs/pattern.svg)",
