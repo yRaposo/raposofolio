67781ea4773ad02fb689c605f219bff155976511
yRaposo <ynyght@gmail.com>
2025-08-26 19:03:43 -0300

    Implement code changes to enhance functionality and improve performance

--- src/components/StrokeText.js
+++ src/components/StrokeText.js
@@ -8,7 +8,7 @@ export default function StrokeText({ text, className = "", repeat = 1, vertical 
                         className="absolute left-1/2 transform -translate-x-1/2 text-transparent font-bebas text-[9rem] font-bold  -translate-y-10"
                         style={{
                             top: `${i * 7}rem`,
-                            backgroundImage: "url(/imgs/ecopattern.svg)",
+                            backgroundImage: "url(/imgs/eco.svg)",
                             backgroundSize: "100%",   // Tamanho do padrão
                             backgroundPosition: `${position}`,
                             backgroundRepeat: "no-repeat",
@@ -24,7 +24,7 @@ export default function StrokeText({ text, className = "", repeat = 1, vertical 
             <div className={`relative ${className}`}>
                 <div className="absolute top-0 left-0 text-transparent font-bebas text-[9rem] font-bold transform -translate-x-1 -translate-y-12"
                     style={{
-                        backgroundImage: "url(/imgs/ecopattern.svg)",
+                        backgroundImage: "url(/imgs/eco.svg)",
                         backgroundSize: "330px",   // Tamanho do padrão
                         backgroundPosition: `${position}`,
                         backgroundRepeat: "repeat-x",
